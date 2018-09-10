2976bdfdae44366afbe30779512ea4d651fc1ab9
Tyler <tjaysherrow@gmail.com>
2018-09-09 19:50:48 -0600

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,42 @@
+# TODO: Add an appropriate license to your skill before publishing.  See
+# the LICENSE file for more information.
+
+# Below is the list of outside modules you'll be using in your skill.
+# They might be built-in to Python, from mycroft-core or from external
+# libraries.  If you use an external library, be sure to include it
+# in the requirements.txt file so the library is installed properly
+# when the skill gets installed later by a user.
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+
+#Error logger
+LOGGER = getLogger(__name__)
+
+# Each skill is contained within its own class, which inherits base methods
+# from the MycroftSkill class.  You extend this class as shown below.
+class MySkillsTest(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(TemplateSkill, self).__init__(name="MySkillsTest")
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, there is no need to override it.  If you DO
+    # need to implement stop, you should return True to indicate you handled
+    # it.
+    #
+    # def stop(self):
+    #    return False
+
+    @intent_handler(IntentBuilder("").require("Query".require("Play")))
+    def handle_query_play_intent(self, message):
+        self.speak_dialog("what.should.we.play.dialog")
+
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return MySkillsTest()
